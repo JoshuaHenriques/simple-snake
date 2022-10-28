533c79dbee350186eab25df493635a4ae9272379
joshuahenriques <joshuahenriques95@gmail.com>
2022-10-27 21:06:29 -0400

    remove .vscode, StateMan & AssetMan

--- include/StateMan.hpp
+++ include/StateMan.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
+#include <stack>
+#include <memory>
+#include <State.hpp>
+
 namespace Engine
 {
     class StateMan {
+        private:
+            // state stack should only be modified at the start of the next update cycle
+            std::stack<std::unique_ptr<State>> m_stateStack;
+            // we don't want to push new state on the stack while the state is executing its update method
+            std::unique_ptr<State> m_newState;
+            bool m_add;
+            bool m_replace;
+            bool m_remove;
         public:
             StateMan(){};
             ~StateMan(){};
+
+            // add to stack or replace current stack
+            void Add(std::unique_ptr<State> toAdd, bool replace = false);
+            // remove current state from state stack
+            void PopCurrent();
+            // state stack will be modified
+            void ProcessStateChange();
+            // to return a unique_ptr to current state object by reference 
+            std::unique_ptr<State>& GetCurrent();
     };
 }
-
---
+++ src/AssetMan.cpp
@@ -0,0 +1,30 @@
+#include "AssetMan.hpp"
+
+Engine::AssetMan::AssetMan() 
+{
+
+}
+
+Engine::AssetMan::~AssetMan() 
+{
+
+}
+
+void Engine::AssetMan::AddTexture(int id, std::string& filePath, bool isRepeated = false)
+{
+
+}
+void Engine::AssetMan::AddFont(int id, const std::string &filePath)
+{
+
+}
+
+const sf::Texture &GetTexture()
+{
+
+}
+
+const sf::Font &GetFont()
+{
+
+}
